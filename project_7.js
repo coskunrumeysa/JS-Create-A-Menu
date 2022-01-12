6f13ea7cbaf0eb5d53e5a19694d080ff9a926f53
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-01-12 10:28:14 +0300

    Add files via upload

---
+++ project_7.js
@@ -0,0 +1,18 @@
+//Beni Yaratan Elbet Yolumu Gösterir.Suara /78.
+// This program written for create a menu at Js.
+hamburger = 'whopper'
+console.log(hamburger)
+icecek = 'kola'
+console.log(icecek)
+patates = 'klasik'
+console.log(patates)
+whopperMenu = hamburger + icecek + patates
+console.log(whopperMenu)
+whopperMenu =  'Menu :' + hamburger + icecek + patates
+console.log(whopperMenu)
+whopperMenu =  'Menu :' + ' '+ hamburger +', '+icecek + ' , ' +patates
+console.log(whopperMenu)
+fiyat = 20
+console.log(fiyat)
+whopperMenu = 'Menu :' + ' '+ hamburger +', '+icecek + ' , ' +patates  +' ,' + 'Fiyat :' + fiyat 
+console.log(whopperMenu)
